813af5af7b2284038b1de1f2321a7360b1400534
Yatoorooo <Egvarakcin@gmail.com>
2023-12-22 20:29:29 +0300

    first js

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let names;
+let surname;
+let age;
+
+names = prompt("What your name?");
+surname = prompt("what tour surname?");
+age = prompt("What tour age?");
+
+user(names);
+user(surname);
+user(age);
+function user(name){
+console.log (name);
+}
+
+let nubmer1, number2, number3, nubmer4, number5;
+nubmer1 = 1;
+number2 = 2;
+number3 = 3;
+nubmer4 = 4;
+number5 = 5;
+
+console.log(nubmer1, number2, number3, nubmer4, number5);
